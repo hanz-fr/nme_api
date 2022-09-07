6bd3c3e31acd7b2455c6b1d1464404945a115c36
recta-rrae <raihanf787@gmail.com>
2022-09-07 22:28:18 +0700

    commit on update

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,19 +4,14 @@ const { User, sequelize } = require("../models");
 // import fastest-validator
 const v = new Validator();
 
-
-
 // GET ALL USER CONTROLLER
-exports.getUser = async (req, res) => {
+exports.getUsers = async (req, res) => {
   const users = await sequelize.query("SELECT * FROM users", {
     model: User,
     mapToModel: true,
   });
   res.status(200).json(users);
 };
-
-
-
 
 
 // CREATE USER CONTROLLER
@@ -33,7 +28,6 @@ exports.createUser = async (req, res) => {
 
     const validate = v.validate(req.body, schema);
 
-
     // check if validation is success or not
     if (validate.length) {
       return res.status(400).json(validate);
@@ -41,30 +35,28 @@ exports.createUser = async (req, res) => {
 
     // find user where email already exist.
     const userEmailExist = await User.findOne({
-        where: { email: req.body.email }
+      where: { email: req.body.email },
     });
 
     // find user where phone already exist.
     const userPhoneExist = await User.findOne({
-        where: { phone: req.body.phone }
-    })
-    
+      where: { phone: req.body.phone },
+    });
 
-    
     // return error message to client if email already exist.
     if (userEmailExist) {
-        return res.status(409).json({
-            status: 'error',
-            message: 'Email already exist'
-        });
+      return res.status(409).json({
+        status: "error",
+        message: "Email already exist.",
+      });
     }
 
     // return error message to client if phone already exist.
     if (userPhoneExist) {
-        return res.status(409).json({
-            status: 'error',
-            message: 'Phone already exist'
-        });
+      return res.status(409).json({
+        status: "error",
+        message: "Phone already exist.",
+      });
     }
 
     var user = await User.create(req.body);
@@ -74,8 +66,64 @@ exports.createUser = async (req, res) => {
       user,
     });
   } catch (error) {
-      console.log(error);
-      res.status(500);
-      res.send({ status: "error", message: "Something went wrong. :(" });
+    console.log(error);
+    res.status(500);
+    res.send({ status: "error", message: "Something went wrong. :(" });
   }
 };
+
+
+// UPDATE USER BY ID CONTROLLER
+exports.updateUser = async (req, res) => {
+  
+  // take the id from req params 
+  const id = req.params.id;
+
+  // check if user is available in db or not.
+  let userExist = await User.findByPk(id);
+
+  if (!userExist) {
+    return res.json({ message: "User not found." });
+  }
+
+  // validate incoming request using fastest-validator
+  const schema = {
+    name: { type: "string", optional: true },
+    age: { type: "number", min: 1, max: 99, optional: true },
+    email: { type: "string", optional: true },
+    address: { type: "string", min: 10, max: 255, optional: true },
+    phone: { type: "number", optional: true },
+  };
+
+  const validate = v.validate(req.body, schema);
+
+  // check if validation is success or not
+  if (validate.length) {
+    return res.status(400).json(validate);
+  }
+
+  userExist = await userExist.update(req.body);
+  res.status(200).json({
+    message: `Successfully updated user with id : ${userExist.id}`,
+    result : userExist
+  })
+};
+
+
+// GET USER BY ID
+exports.getUser = async (req, res) => {
+    const id = req.params.id;
+
+    // check if user exist or not.
+    const user = await User.findByPk(id);
+
+    // if not, return error
+    if (!user) {
+        return res.json({ message: "User not found." });
+    }
+
+    res.status(200).json({
+        message: `Displaying user with id : ${id}`,
+        result: user
+    })
+} 
--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,9 @@ const router = express.Router();
 const controller = require('../controllers/userController');
 
 
-router.get('/', controller.getUser); // GET ROUTE
-router.post('/', controller.createUser); // POST ROUTE
+router.get('/', controller.getUsers); // GET ALL USER ROUTE
+router.post('/', controller.createUser); // POST USER ROUTE
+router.put('/:id', controller.updateUser); // UPDATE USER ROUTE
+router.get('/:id', controller.getUser); // GET USER ROUTE
 
 module.exports = router;
